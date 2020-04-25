d3b580e38423847288e19633c404625ab969e58a
vzucher <vzucher@gmail.com>
2020-04-25 15:36:18 -0300

    lack frontend

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,46 @@
+class FlatsController < ApplicationController
+  before_action :find_flat, only: %i[show edit update destroy]
+
+  def index
+    @flats = Flat.all
+  end
+
+  def show; end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+    redirect_to flat_path(@flat)
+  end
+
+  def edit; end
+
+  def update
+    @flat.update(flat_params)
+  end
+
+  def destroy
+    @flat.destroy
+    redirect_to flats_path
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(
+      :name,
+      :address,
+      :description,
+      :price_per_night,
+      :number_of_guests
+    )
+  end
+
+  def find_flat
+    @flat = Flat.find(params[:id])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+3.times do
+  Flat.create(
+    name: Faker::Coffee.blend_name,
+    address: Faker::Address.street_name,
+    description: Faker::Quote.yoda,
+    price_per_night: "R$#{(0..99).to_a.sample}",
+    number_of_guests: (0..7).to_a.sample
+  )
+end
